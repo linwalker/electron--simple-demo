388b6790483fc1c1c30eff6a98455d1fd5928dc5
linwaler <wzamate@163.com>
2017-05-31 21:12:46 +0800

    add page

---
+++ assets/import.js
@@ -0,0 +1,7 @@
+const links = document.querySelectorAll('link[rel="import"]');
+
+Array.prototype.forEach.call(links, function(link) {
+    let template = link.import.querySelector('.task-template');
+    let clone = document.importNode(template.content, true);
+    document.querySelector('.content').appendChild(clone);
+})
